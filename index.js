59ab7359255894ec4b72885d0ca98d1daaaecd7b
Long Tran <57053685+baolongt@users.noreply.github.com>
2022-11-27 12:52:26 +0700

    fix

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const app = express();
 
 app.get("/", async function (req, res) {
 	const { orderInfo, amount } = req.query;
-	const payment = {
-		orderInfo,
-		amount
-	};
-	const payUrl = await createPaymentRequest(payment);
-	res.send(payUrl);
+	if (!orderInfo || !amount) res.send("must provide orderInfo and amount");
+	else {
+		const payment = {
+			orderInfo,
+			amount
+		};
+
+		const payUrl = await createPaymentRequest(payment);
+		res.send(payUrl);
+	}
 });
 
 app.listen(8080, () => {
